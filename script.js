30afb43453a5eb3343e8d65a4fc011873896f6c7
radwaGhanem <155246119+radwaGhanem@users.noreply.github.com>
2025-05-23 19:14:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 function showMessage() {
   alert("Hi Radwa! Your server is working!");
 }
+const sections = document.querySelectorAll('.section');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+      observer.unobserve(entry.target);
+    }
+  });
+}, { threshold: 0.1 });
+
+sections.forEach(section => {
+  observer.observe(section);
+});
